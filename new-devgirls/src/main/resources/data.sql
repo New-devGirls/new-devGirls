70fa22afa97fcc662d481ae610d0506e5cc8766d
Mariana Mercier <49447307+marianamercier@users.noreply.github.com>
2021-05-21 20:36:01 -0300

    Adiciona arquivo para popular banco de dados

---
+++ new-devgirls/src/main/resources/data.sql
@@ -0,0 +1,83 @@
+-- Categoria Eletrodoméstico
+
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("10101", “Air fryer”, 500, "Eletro", "https://images2.imgbox.com/1a/7f/4tjKlONr_o.jpeg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("10102", “Batedeira”, 200, "Eletro", "https://images2.imgbox.com/1e/ae/ocjqWqNt_o.jpg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("10103", “Fogão”, 100, "Eletro", "https://images2.imgbox.com/3a/8c/L3CW2SxQ_o.jpeg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("10104", “Forno Elétrico”, 200, "Eletro", "https://images2.imgbox.com/bb/b1/i1UtXrur_o.jpg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("10105", “Geladeira”, 1000, "Eletro", "https://images2.imgbox.com/96/8d/JFR92qnO_o.jpg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("10106", “Liquidificador”, 80, "Eletro", "https://images2.imgbox.com/52/38/xbhNSBqb_o.jpg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("10107", “Microondas”, 600, "Eletro", "https://images2.imgbox.com/8e/c8/KlnJ9mPq_o.jpeg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("10108", “Mixer”, 150, "Eletro", "https://images2.imgbox.com/11/4f/K6sL5Jjy_o.jpg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("10109", “Panela de Pressão Elétrica”, 300, "Eletro", "https://images2.imgbox.com/ef/39/laAzVOhC_o.jpg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("10110", “Processador”, 200, "Eletro", "https://images2.imgbox.com/c1/0c/U7zTfYrk_o.jpg") ;
+
+-- Categoria Móveis
+
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("20101", “Mesa de Cabeceira”, 200, "Móveis", "https://images2.imgbox.com/92/4e/OyPGPbFa_o.jpeg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("20102", “Cama Baú”, 1.000, "Móveis", "https://images2.imgbox.com/40/75/KSjAIEe4_o.jpg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("20103", “Cômoda”, 700, "Móveis", "https://images2.imgbox.com/bd/1d/KE0Jhg6N_o.jpg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("20104", “Escrivaninha”, 500, "Móveis", "https://images2.imgbox.com/28/ed/3vsXBpaM_o.jpg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("20105", “Estante”, 200, "Móveis", "https://images2.imgbox.com/0a/93/bVYIoJI4_o.jpg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("20106", “Mesa de Jantar com 6 cadeiras”, 1500, "Móveis", "https://images2.imgbox.com/a1/7c/v8yFrQNQ_o.jpg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("20107", “Poltrona”, 300, "Móveis", "https://images2.imgbox.com/4f/ef/aNTdhxOo_o.jpg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("20108", “Puff”, 200, "Móveis", "https://images2.imgbox.com/78/d8/MtkXCF3l_o.jpg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("20109", “Sofá”, 2000, "Móveis", "https://images2.imgbox.com/80/ac/lf4fo55I_o.jpg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("20110", “Rack”, 800, "Móveis", "https://images2.imgbox.com/43/55/S5bYF2a6_o.jpg") ;
+
+-- Categoria Brinquedos
+
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("30101", “Baby Alive”, 199, "Brinquedos", "https://images2.imgbox.com/1a/3a/2cBlZLjH_o.jpeg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("30102", “Barbie Fashion”, 99, "Brinquedos", "https://images2.imgbox.com/08/7e/Bl1AmHR1_o.jpg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("30103", “Caminhão Truck Way”, 50, "Brinquedos", "https://images2.imgbox.com/6a/c4/coyb7dDN_o.jpeg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("30104", “Dinossauro Barrigudo”, 70, "Brinquedos", "https://images2.imgbox.com/47/59/6i1LM7c9_o.jpg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("30105", “Homem Aranha”, 80, "Brinquedos", "https://images2.imgbox.com/eb/02/hQO2stze_o.jpg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("30106", “Furby”, 200, "Brinquedos", "https://images2.imgbox.com/16/ca/fG6FctRq_o.jpg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("30107", “Cozinha de Boneca”, 120, "Brinquedos", "https://images2.imgbox.com/78/74/ltAECMrx_o.jpg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("30108", “Massinha de Modelar”, 45, "Brinquedos", "https://images2.imgbox.com/77/df/zOwd6bCO_o.jpeg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("30109", “Pelúcia Divertida”, 40, "Brinquedos", "https://images2.imgbox.com/73/10/Y3cJdrqM_o.jpg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("30110", “Pogobol”, 100, "Brinquedos", "https://images2.imgbox.com/cd/36/6O35ePqN_o.jpg") ;
+
+-- Categoria Ferramentas
+
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("40101", “Alicate”, 70, "Ferramentas", "https://images2.imgbox.com/ae/45/eMrhc1gb_o.jpeg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("40102", “Bomba de Ar”, 40, "Ferramentas", "https://images2.imgbox.com/8c/18/PPQt3as3_o.jpeg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("40103", “Conjunto de Chave de Fendas”, 100, "Ferramentas", "https://images2.imgbox.com/19/12/CU60VDb4_o.jpg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("40104", “Furadeira”, 199, "Ferramentas", "https://images2.imgbox.com/dc/f1/SC8TArBK_o.jpg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("40105", “Kit de Ferramentas”, 150, "Ferramentas", "https://images2.imgbox.com/92/d0/7oKFPrgr_o.jpg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("40106", “Lixadeira”, 250, "Ferramentas", "https://images2.imgbox.com/a3/02/IwGDwUBb_o.jpg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("40107", “Makita”, 300, "Ferramentas", "https://images2.imgbox.com/6c/48/TWqJNbDl_o.jpg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("40108", “Martelo”, 80, "Ferramentas", "https://images2.imgbox.com/f0/06/OpNBQD2A_o.jpg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("40109", “Multímetro”, 100, "Ferramentas", "https://images2.imgbox.com/62/6b/HBPKB3rl_o.jpeg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("40110", “Trena”, 30, "Ferramentas", "https://images2.imgbox.com/2d/58/Y2AhPqnK_o.jpg") ;
+
+
+-- Categoria Tecnologia
+
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("50101", “Fone de Ouvido”, 200, "Tecnologia", "https://images2.imgbox.com/b4/f4/Z6AMgK6m_o.jpeg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("50102", “IPhone 12”, 6000, "Tecnologia", "https://images2.imgbox.com/cd/fd/Uxiy1E5T_o.jpg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("50103", “Monitor”, 700, "Tecnologia", "https://images2.imgbox.com/46/25/97kVRNgq_o.jpg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("50104", “Mouse sem fio”, 100, "Tecnologia", "https://images2.imgbox.com/9d/85/ahSttlev_o.jpg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("50105", “Notebook”, 4000, "Tecnologia", "https://images2.imgbox.com/d3/0f/201xhltv_o.jpg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("50106", “Smart Watch”, 899, "Tecnologia", "https://images2.imgbox.com/4a/0d/mtawPMjJ_o.jpg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("50107", “Roteador”, 249, "Tecnologia", "https://images2.imgbox.com/a4/af/jOPormXt_o.jpg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("50108", “Samsung Galaxy A21”, 1200, "Tecnologia", "https://images2.imgbox.com/a1/53/frOhKaIk_o.jpg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("50109", “Tablet ”, 900, "Tecnologia", "https://images2.imgbox.com/1a/ed/SUol4KlS_o.jpg") ;
+INSERT INTO product (barcode, name, price, category, image_url) VALUES ("50110", “Teclado Mecânico”, 450, "Tecnologia", "https://images2.imgbox.com/de/bf/Z3uJyV9l_o.jpg") ;
+
+-- Cadastro de Clientes
+
+INSERT INTO client (name, cpf, password) VALUES ('Gizele Rodrigues', '12345678912', 'newcodegirls1');
+INSERT INTO client (name, cpf, password) VALUES ('Mariana Mercier', '12345678913', 'newcodegirls2');
+INSERT INTO client (name, cpf, password) VALUES ('Priscila Medeiros', '12345678914', 'newcodegirls3');
+INSERT INTO client (name, cpf, password) VALUES ('Rosilene Signorelli', '12345678915', 'newcodegirls4');
+INSERT INTO client (name, cpf, password) VALUES ('Yarin Alcantara', '12345678916', 'newcodegirls5');
+
+
+-- Wishlist
+
+INSERT INTO wishlist (id_client, id_product) VALUES (1, 10);
+INSERT INTO wishlist (id_client, id_product) VALUES (1, 20);
+INSERT INTO wishlist (id_client, id_product) VALUES (2, 30);
+INSERT INTO wishlist (id_client, id_product) VALUES (2, 40);
+INSERT INTO wishlist (id_client, id_product) VALUES (3, 20);
+INSERT INTO wishlist (id_client, id_product) VALUES (3, 1);
