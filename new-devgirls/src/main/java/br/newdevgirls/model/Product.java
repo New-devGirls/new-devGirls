f8f11ada953b0fe5fa8017968ea65265ad46a9c9
priims <priiscilams@hotmail.com>
2021-05-22 01:17:03 -0300

    Correção cadastro de produto na wishlist

--- new-devgirls/src/main/java/br/newdevgirls/model/Product.java
+++ new-devgirls/src/main/java/br/newdevgirls/model/Product.java
@@ -12,6 +12,8 @@ import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.sun.istack.NotNull;
 
 import lombok.AllArgsConstructor;
@@ -19,7 +21,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "product")
@@ -52,16 +53,61 @@ public class Product {
 	  @ManyToMany(fetch = FetchType.LAZY, mappedBy = "products")
 	  private List<Client> clients = new ArrayList<>();
 
-	  public Long getId() {
-	    return id;
-	  }
+	public Long getId() {
+		return id;
+	}
 
-	  public void setId(Long id) {
-	    this.id = id;
-	  }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-	  public String getBarcode() {
-	    return barcode;
-	  }
+	public String getBarcode() {
+		return barcode;
+	}
 
+	public void setBarcode(String barcode) {
+		this.barcode = barcode;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public String getImage_url() {
+		return image_url;
+	}
+
+	public void setImage_url(String image_url) {
+		this.image_url = image_url;
+	}
+
+	@JsonIgnore
+	public List<Client> getClients() {
+		return clients;
+	}
+
+	@JsonProperty
+	public void setClients(List<Client> clients) {
+		this.clients = clients;
+	}
 }
